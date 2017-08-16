0132fbbfd681ae89a50fcd522f2d50c286ccbfe4
Angel Talavera <atalavera@mimecast.com>
2017-08-16 21:27:08 +0100

    R - changed class for interface

--- src/app/app.user.account.ts
+++ src/app/app.user.account.ts
@@ -1,15 +1,7 @@
-export class UserAccount {
+export interface UserAccount {
   id: number;
   includeAdult: boolean;
   language: string;
   name: string;
   username: string;
-
-  constructor(id: number, includeAdult: boolean, language: string, name: string, username: string) {
-    this.id = id;
-    this.includeAdult = includeAdult;
-    this.language = language;
-    this.name = name;
-    this.username = username;
-  }
 }
