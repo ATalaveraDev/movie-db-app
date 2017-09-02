74d0c570662f2835956aa4a5be757c1becb1d605
Angel Talavera <atalavera@mimecast.com>
2017-09-02 13:26:31 +0100

    I - added movie poster to detail

--- src/app/movies/detail/detail.component.ts
+++ src/app/movies/detail/detail.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Movie } from '../list/movie/movie.model';
+import { Constants } from '../../app.constants';
+import { AppService } from '../../app.service';
 
 @Component({
   selector: 'movie-detail',
   templateUrl: 'detail.component.html'
 })
-export class MovieDetailComponent {
+export class MovieDetailComponent implements OnChanges {
   @Input() selectedMovie: Movie;
+  movie;
 
-  constructor() {}
+  constructor(private httpClient: HttpClient, private appService: AppService) { }
+
+  ngOnChanges() {
+    this.httpClient.get(Constants.END_POINT + '/movie/' + this.selectedMovie.id + '?api_key=' + this.appService.getApiKey())
+        .map((response: any) => this.movie = response)
+        .subscribe();
+  }
 }
+
+
