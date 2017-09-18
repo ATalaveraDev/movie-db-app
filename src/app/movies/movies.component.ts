56feeed8063e305ba0e8b3cd47ab93fe936e5eb5
Angel Talavera <atalavera@mimecast.com>
2017-09-18 20:45:38 +0100

    R - removed unnecessary code

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
 import { Movie } from './list/movie/movie.model';
+import { MoviesService } from '../movies.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent {
   selectedMovie: Movie;
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) {
+    this.moviesService.getMovies();
+  }
 
   onClickMovie(movie) {
     this.selectedMovie = movie;
