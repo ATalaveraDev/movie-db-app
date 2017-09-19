70b1d8afb5e5bc632bd0d598eb2ce19c1b3c8289
Angel Talavera <atalavera@mimecast.com>
2017-09-19 09:12:10 +0100

    R - used ReplaySubject for declare empty list

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 
 import { Constants } from './app.constants';
